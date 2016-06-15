2902e91880d0cdb485d1fb371e309b6918c41625
Ryan McHenry <ryanmmchenry@gmail.com>
2016-06-14 22:11:06 -0700

    adding particle js to homepage

--- private/js/client/page/home.js
+++ private/js/client/page/home.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", initHomePage.call(document));
 
-function initGameSearchPage (e) {
+function initHomePage (e) {
   var page = this;
+
+  loadParticles();
+}
+
+
+function loadParticles () {
+  console.log('trying to load particles');
+  particlesJS.load('particles-js', '/js/utils/particlesjs-config-min.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 }
