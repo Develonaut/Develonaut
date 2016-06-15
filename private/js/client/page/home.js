0e8f65442df52e5b1fb0d37dc7f0e3eafcd3ff6d
Ryan McHenry <ryanmmchenry@gmail.com>
2016-06-15 14:45:31 -0700

    added new layout styling and blur effects

--- private/js/client/page/home.js
+++ private/js/client/page/home.js
@@ -2,10 +2,25 @@ document.addEventListener("DOMContentLoaded", initHomePage.call(document));
 
 function initHomePage (e) {
   var page = this;
+      logo = page.querySelector("[data-role=logo]");
 
   loadParticles();
+  logo.addEventListener("click", addWooHoo);
 }
 
+function addWooHoo () {
+  var self = this;
+
+  // Prevent fast clicking
+  if (self.classList.contains("woo-hoo")) {
+    return;
+  }
+
+  self.classList.toggle("woo-hoo");
+  _.delay(function() {
+    self.classList.toggle("woo-hoo");
+  }, 500);
+}
 
 function loadParticles () {
   console.log('trying to load particles');
